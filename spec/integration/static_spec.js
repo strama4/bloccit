97c1ba1d223a75e697f64cf27bb53a8103dc38f0
Luke Strama <strama@ualberta.ca>
2018-10-22 10:06:04 -0600

    intro-express-assignment

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -11,4 +11,13 @@ describe("routes : static", () => {
             });
         });
     });
+    describe("GET /marco", () => {
+        it("should return status code 200 and body of 'polo'", (done) => {
+            request.get(base + 'marco', (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                expect(body).toBe('polo');
+                done();
+            });
+        }); 
+    });
 });
