3d8414a492ec264b4dee7419529a45642328f87e
Luke Strama <strama@ualberta.ca>
2018-10-22 14:55:52 -0600

    WIP deploy to production

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const app = express();
 
 module.exports = app;
 
+const appConfig = require('./config/main-config');
 const routeConfig = require("./config/route-config.js");
 
+appConfig.init();
 routeConfig.init(app);
