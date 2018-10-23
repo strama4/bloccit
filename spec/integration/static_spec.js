c1eb58368bc1844b715eb2e8e33319b278a70e42
Luke Strama <strama@ualberta.ca>
2018-10-23 13:50:18 -0600

    view-controllers-assignment

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -12,4 +12,14 @@ describe("routes : static", () => {
             });
         });
     });
+    
+    describe("GET /about", () => {
+        it("should return status code 200 and contain 'About Us' in the body of the response" , (done) => {
+            request.get(base + 'about', (err, res, body) => {
+                expect(res.statusCode).toBe(200);
+                expect(body).toContain('About Us');
+                done();
+            })
+        })
+    })
 });
--- src/routes/static.js
+++ src/routes/static.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const staticController = require('../controllers/staticControllers');
 
 router.get('/', staticController.index);
+router.get('/about', staticController.about);
 
 module.exports = router;
