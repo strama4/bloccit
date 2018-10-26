107839729b60d2050622c1f41607ae4e39c097fb
Luke Strama <strama@ualberta.ca>
2018-10-26 10:53:43 -0600

    fixed edit bug

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -56,9 +56,9 @@ module.exports = {
     update(req, res, next) {
         topicQueries.updateTopic(req.params.id, req.body, (err, topic) => {
             if (err || topic == null) {
-                res.redirect(404, `topics/${req.params.id}/edit`);
+                res.redirect(404, `/topics/${req.params.id}/edit`);
             } else {
-                res.redirect(`topics/${topic.id}`);
+                res.redirect(`/topics/${topic.id}`);
             }
         })
     }
