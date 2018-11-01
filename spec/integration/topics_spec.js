09bc4dae37e11febe48734774bc5efb82927dc32
Luke Strama <strama@ualberta.ca>
2018-11-01 09:47:10 -0600

    validations-assignment

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -66,6 +66,26 @@ describe('routes : topics', () => {
                     });
                 });
         });
+        it('should not create a new topic that fails validations', (done) => {
+            const options = {
+                url: `${base}create`,
+                form: {
+                    title: 'a',
+                    description: 'b'
+                }
+            }
+            request.post(options, (err, res, body) => {
+                Topic.findOne({where: {title: 'a'}})
+                .then((topic) => {
+                    expect(topic).toBeNull();
+                    done();
+                })
+                .catch((err) => {
+                    console.log(err);
+                    done();
+                })
+            });
+        });
     });
     describe('GET /topics/:id', () => {
         it('should render a view with the selected topic', (done) => {
