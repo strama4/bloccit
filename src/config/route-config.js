a86fc16feb8c899bb41f547c42419abd853e9ebb
Luke Strama <strama@ualberta.ca>
2018-11-02 11:58:00 -0600

    authentication-checkpoint

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -3,9 +3,11 @@ module.exports = {
         const staticRoutes = require("../routes/static");
         const topicRoutes = require("../routes/topics");
         const postRoutes = require('../routes/posts');
+        const userRouters = require('../routes/users');
 
         app.use(staticRoutes);
         app.use(topicRoutes);
         app.use(postRoutes);
+        app.use(userRouters);
     }
 }
---
+++ src/db/queries.users.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcryptjs');
+const User = require('./models').User;
+
+module.exports = {
+    createUser(newUser, callback) {
+        const salt = bcrypt.genSaltSync();
+        const hashedPassword = bcrypt.hashSync(newUser.password, salt);
+        User.create({
+            email: newUser.email,
+            password: hashedPassword
+        })
+        .then((user) => {
+            callback(null, user);
+        })
+        .catch((err) => {
+            callback(err);
+        })
+    },
+    getUser(id, callback) {
+        User.findById(id)
+        .then((user) => {
+            callback(null, user);
+        })
+        .catch((err) => {
+            callback(err);
+        })
+    }
+}
